00582dae06ae881e85518ab2ea382efd0cf17678
akki <akki@192.168.1.5>
2023-09-23 22:29:39 +0530

    This is first try to push the code.

---
+++ assignment1R/src/assignment1RPKG/TC1.java
@@ -0,0 +1,14 @@
+package assignment1RPKG;
+
+public class TC1 {
+
+	private void first() {
+		// TODO Auto-generated method stub
+		System.out.println("Test message....");
+	}
+	
+	public static void main(String[] args) {
+		TC1 ref = new TC1();
+		ref.first();
+	}
+}
