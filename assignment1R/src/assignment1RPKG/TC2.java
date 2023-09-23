cc81a0a73fbebec8f420172523227107697e0cd5
akki <akki@192.168.1.5>
2023-09-24 04:21:22 +0530

    This TC2 commit 24th-Sep-2023

--- assignment1R/src/assignment1RPKG/TC2.java
+++ assignment1R/src/assignment1RPKG/TC2.java
@@ -1,14 +1,13 @@
 package assignment1RPKG;
 
-public class TC1 {
-
+public class TC2 {
 	private void first() {
 		// TODO Auto-generated method stub
-		System.out.println("Test message from git....");
+		System.out.println("I am second file....");
 	}
 	
 	public static void main(String[] args) {
-		TC1 ref = new TC1();
+		TC2 ref = new TC2();
 		ref.first();
 	}
 }
